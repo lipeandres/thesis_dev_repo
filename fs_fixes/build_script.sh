3b3f3754c5ad4fc54600f886f6b3255cb9a2e686
Felipe Navarro <lipeandres@gmail.com>
2012-10-29 18:11:50 -0500

    fixed the post-build script

--- fs_fixes/build_script.sh
+++ fs_fixes/build_script.sh
@@ -2,4 +2,3 @@
 cp ../fs_fixes/profile ./output/target/etc/profile
 cp ../fs_fixes/fstab ./output/target/etc/fstab
 cp ../fs_fixes/usbfix ./output/target/usr/bin/usbfix
-ln -s ./output/target/mnt ./output/root/sdcard
