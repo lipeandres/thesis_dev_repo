5dcf1bfcb3707959913a1fdf00ccdbdf3e28d35b
Daniel Cardenas <uncardenas@gmail.com>
2012-10-29 11:15:21 -0500

    modified build script working buildroot with flush

--- fs_fixes/build_script.sh
+++ fs_fixes/build_script.sh
@@ -2,3 +2,4 @@
 cp ../fs_fixes/profile ./output/target/etc/profile
 cp ../fs_fixes/fstab ./output/target/etc/fstab
 cp ../fs_fixes/usbfix ./output/target/usr/bin/usbfix
+ln -s ./output/target/mnt ./output/root/sdcard
